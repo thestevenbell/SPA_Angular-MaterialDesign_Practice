26147e9938ffebd7c104da2bfcb498ea9163b3d6
Steven Bell <thestevenbell@gmail.com>
2016-04-28 15:20:02 -0400

    complete the build

--- js/materialApp.js
+++ js/materialApp.js
@@ -1,5 +1,19 @@
 // You must include the dependency on 'ngMaterial'
-
-angular.module('materialApp', ['ngMaterial']);
-
 console.log("materialApp.js linked.");
+
+angular
+    .module('materialApp', ['ngMaterial', 'users'])
+    .config(function($mdThemingProvider, $mdIconProvider){
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+            .icon("share"      , "./assets/svg/share.svg"       , 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+            .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+            .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+            .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+    });
+
--- js/users/Users.js
+++ js/users/Users.js
@@ -1,1 +1,8 @@
+(function(){
+  'use strict';
 
+  // Prepare the 'users' module for subsequent registration of controllers and delegates
+  angular.module('users', [ 'ngMaterial' ]);
+
+
+})();
